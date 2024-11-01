b9ba0fd1629042b6078d1a95e55285ed47c1f72c
marting <martin.gamboa@usach.cl>
2024-11-01 12:50:46 -0300

    Commit 2, fix

--- src/test/java/com/example/backend_martin_gamboa/BackendMartinGamboaApplicationTests.java
+++ src/test/java/com/example/backend_martin_gamboa/BackendMartinGamboaApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.backend_martin_gamboa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendMartinGamboaApplicationTests {
 
 	@Test
